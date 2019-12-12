5510d5f3dc5e609c6fd425ca979d46d571db06d9
quan200699 <maplestory2012vn@gmail.com>
2019-12-12 11:51:23 +0700

    type of question service

---
+++ src/app/service/type-of-question.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {TypeOfQuestion} from '../model/type-of-question';
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TypeOfQuestionService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  listTypeOfQuestion(): Observable<TypeOfQuestion[]> {
+    return this.http.get<TypeOfQuestion[]>(API_URL + '/typeOfQuestions');
+  }
+
+  getTypeOfQuestion(id: number): Observable<TypeOfQuestion> {
+    return this.http.get<TypeOfQuestion>(API_URL + `typeOfQuestions/${id}`);
+  }
+}
