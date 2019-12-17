812f376dac43ce8cbfcfe93f943217cee3425f8a
quan200699 <maplestory2012vn@gmail.com>
2019-12-17 12:55:12 +0700

    remove faIcon in navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {UserToken} from '../../model/user-token';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../../service/authentication.service';
-import {faHome, faList, faQuestion, faSignInAlt, faSignOutAlt, faUserPlus} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +11,6 @@ import {faHome, faList, faQuestion, faSignInAlt, faSignOutAlt, faUserPlus} from 
 export class NavbarComponent {
   currentUser: UserToken;
   public isMenuCollapsed = true;
-  homeIcon = faHome;
-  questionIcon = faQuestion;
-  categoryIcon = faList;
-  logoutIcon = faSignOutAlt;
-  loginIcon = faSignInAlt;
-  registerIcon = faUserPlus;
 
   constructor(
     private router: Router,
