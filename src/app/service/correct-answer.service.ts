e88804aaafbf42f110b766dc871f484694c15d64
quan200699 <51079189+quan200699@users.noreply.github.com>
2019-12-23 10:33:47 +0700

    correct answer service

--- src/app/service/correct-answer.service.ts
+++ src/app/service/correct-answer.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {CorrectAnswer} from '../model/correct-answer';
+
+const API_URL = `${environment.apiUrl}`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class CorrectAnswerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  listCorrectAnswerByQuestion(questionId: number): Observable<CorrectAnswer[]> {
+    return this.http.get<CorrectAnswer[]>(API_URL + `/correctAnswers/${questionId}`);
+  }
+
+  createCorrectAnswer(correctAnswer: CorrectAnswer): Observable<CorrectAnswer> {
+    return this.http.post<CorrectAnswer>(API_URL + '/correctAnswers', correctAnswer);
+  }
+
+  updateCorrectAnswer(correctAnswer: CorrectAnswer, id: number): Observable<CorrectAnswer> {
+    return this.http.put<CorrectAnswer>(API_URL + `/correctAnswers/${id}`, correctAnswer);
+  }
+
+  deleteCorrectAnswer(id: number): Observable<CorrectAnswer> {
+    return this.http.delete<CorrectAnswer>(API_URL + `/correctAnswers/${id}`);
+  }
+
+  getCorrectAnswer(id: number): Observable<CorrectAnswer> {
+    return this.http.get<CorrectAnswer>(API_URL + `/getCorrectAnswer/${id}`);
+  }
 }
