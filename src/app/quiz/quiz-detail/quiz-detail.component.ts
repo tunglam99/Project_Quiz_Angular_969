f49ca4bb7087a85e46e20f8b9a6af2ac233d6f75
quan200699 <maplestory2012vn@gmail.com>
2019-12-24 21:53:03 +0700

    remove question to quiz

--- src/app/quiz/quiz-detail/quiz-detail.component.ts
+++ src/app/quiz/quiz-detail/quiz-detail.component.ts
@@ -14,6 +14,7 @@ export class QuizDetailComponent implements OnInit {
   questionList: Question[] = [];
   quizId: number;
   sub: Subscription;
+  currentQuestion: Question;
 
   constructor(private quizService: QuizService,
               private questionService: QuestionService,
@@ -28,11 +29,40 @@ export class QuizDetailComponent implements OnInit {
 
   }
 
+  getQuestionDetail(id: number) {
+    this.questionService.getQuestion(id).subscribe(result => {
+      this.currentQuestion = result;
+    }, error => {
+      console.log(error);
+    });
+  }
+
   getQuestionList() {
     this.questionService.findAllQuestionByQuiz(this.quizId).subscribe(result => {
       this.questionList = result;
     }, error => {
       console.log(error);
     });
+  }
+
+  removeQuestionFromQuiz(questionId: number) {
+    this.getQuestionDetail(questionId);
+    if (this.currentQuestion.id != null) {
+      console.log(this.currentQuestion);
+      const question: Question = {
+        id: this.currentQuestion.id,
+        content: this.currentQuestion.content,
+        typeOfQuestion: this.currentQuestion.typeOfQuestion,
+        category: this.currentQuestion.category,
+        correctAnswer: this.currentQuestion.correctAnswer,
+        status: this.currentQuestion.status,
+        quiz: null
+      };
+      this.questionService.updateQuestion(questionId, question).subscribe(() => {
+        this.getQuestionList();
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
