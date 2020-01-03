2d86befb519e99d350b8c92f2c14771e46cc3910
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-01-03 20:54:32 +0700

    remove correctanswer and answer property in question:

--- src/app/model/question.ts
+++ src/app/model/question.ts
@@ -2,9 +2,7 @@ export interface Question {
   id: number;
   content: string;
   status: boolean;
-  correctAnswer?: any[];
   typeOfQuestion?: any;
   category?: any;
-  answers?: any[];
   quiz?: any;
 }
