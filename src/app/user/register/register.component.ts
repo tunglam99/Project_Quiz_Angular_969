5a6318412a93a27e18ac4168c07528fd6345f450
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-01-07 21:59:18 +0700

    validate register

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -10,13 +10,13 @@ import {Router} from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  failMessage = '';
   registerForm: FormGroup = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
     password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
-    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    confirmPassword: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
   });
+  isSubmitted = false;
 
   constructor(private userService: UserService,
               private router: Router) {
@@ -26,6 +26,7 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
+    this.isSubmitted = true;
     const user: User = {
       id: this.registerForm.value.id,
       username: this.registerForm.value.username,
@@ -37,8 +38,10 @@ export class RegisterComponent implements OnInit {
     this.userService.register(user).subscribe(() => {
       this.registerForm.reset();
       this.router.navigate(['register-success']);
-    }, () => {
-      this.failMessage = 'Đăng ký thất bại';
     });
+  }
+
+  isCorrectConfirmPassword(password: string, confirmPassword: string) {
+    return password === confirmPassword;
   }
 }
