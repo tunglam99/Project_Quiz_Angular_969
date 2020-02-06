c59c3e3d55a0829ee4dd70e59e7f3d0ed24911b1
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-02-06 15:45:26 +0700

    student class component

---
+++ src/app/student-class/student-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-class',
+  templateUrl: './student-class.component.html',
+  styleUrls: ['./student-class.component.css']
+})
+export class StudentClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
